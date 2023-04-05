35868c7c0b5640c5d51d74b80062994dd65b76fb
Carlos Santos <carlos.santos@globalsys.com.br>
2023-04-05 16:44:25 -0300

    Add project files.

---
+++ GeekShopping.ProductAPI/Model/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions option) : base(option)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Web.Data.Dtos;
+
+namespace GeekShopping.Web.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetAll();
+        Task<ProductDto> GetById(long id);
+        Task<ProductDto> Create(ProductDto dto);
+        Task<ProductDto> Update(ProductDto dto);
+        Task<bool> Delete(long id);
+    }
+}
